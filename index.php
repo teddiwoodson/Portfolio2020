46b66f87ecca411189babf74cae90b641c245f24
Teddi Woodson <teddiwood97@ufl.edu>
2020-05-31 00:36:52 -0400

    Update jumbotron and page order

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php get_header(); ?>
 
-<!-- ======================= Blog Posts ==================-->
-
-  <section class="row justify-content-center">
-    <?php if(have_posts()){
-      while(have_posts()){
-        the_post(); ?>
-        <div class="grid">
-          <a href="<?php the_permalink(); ?>">
-            <figure class="effect-steve">
-              <?php  the_post_thumbnail('medium'); ?>
-              <figcaption>
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_excerpt(); ?></p>
-              </figcaption>
-            </figure>
-          </a>
-        </div>
-      <?php } // ends while loop
-    } // ends if statement
-    ?>
-    </section>
-
     <!--======================= Header ====================-->
     	<main class="">
     		<div class="jumbotron jumbotron-fluid">
@@ -123,8 +101,30 @@
     			  </div></a>
     		</div>
     	</div>
-    	<a type="button" class="btn btn-primary btn-lg">Click Here to View My Portfolio</a>
     </div>
+
+
+    <!-- ======================= Blog Posts ==================-->
+
+      <section class="row justify-content-center">
+        <?php if(have_posts()){
+          while(have_posts()){
+            the_post(); ?>
+            <div class="grid">
+              <a href="<?php the_permalink(); ?>">
+                <figure class="effect-steve">
+                  <?php  the_post_thumbnail('medium'); ?>
+                  <figcaption>
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php the_excerpt(); ?></p>
+                  </figcaption>
+                </figure>
+              </a>
+            </div>
+          <?php } // ends while loop
+        } // ends if statement
+        ?>
+        </section>
 
     <!--======================= Resume ====================-->
 
