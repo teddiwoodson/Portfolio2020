6c0889ed22f3cd711625ba5c468edd5324a603da
Teddi Woodson <teddiwood97@ufl.edu>
2020-05-30 23:10:00 -0400

    Update head tag info

--- functions.php
+++ functions.php
@@ -7,15 +7,8 @@ adds stylesheet and javascript files
 =================*/
 
   function custom_theme_scripts(){
-    //Bootstrap Integration
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
-
     //Main CSS
     wp_enqueue_style('main-styles', get_stylesheet_uri());
-
-    //javascript file
-    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
-    wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
   }
 
   add_action('wp_enqueue_scripts', 'custom_theme_scripts');
--- header.php
+++ header.php
@@ -3,13 +3,12 @@
 <head>
   <meta charset="utf-8">
 
-	<title>Teddi Woodson</title>
+  <title><?php bloginfo('name');?></title>
 
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+<!--link to our style.css file -->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"/>
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
-
-	<link type="text/css" rel="stylesheet" href="styles.css"/>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -18,8 +17,6 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-  <script src="js/html5shiv-printshiv.min.js"></script>
 
   <?php wp_head(); ?>
 </head>
