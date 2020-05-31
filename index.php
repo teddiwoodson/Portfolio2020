9e22bdb1ad9c3e1ff7dc6e1c9b17350cede31e43
Teddi Woodson <teddiwood97@ufl.edu>
2020-05-30 23:00:37 -0400

    Add index styles

--- index.php
+++ index.php
@@ -1,35 +1,25 @@
 <?php get_header(); ?>
 
-  <div class="container">
-    <div class="row justify-content-center" id="post-row">
-      <?php if(have_posts()){
-        while(have_posts()){
-          the_post(); ?>
-          <div class="col-lg-3 col-md-6 col-sm-12" id="post">
+<!-- ======================= Blog Posts ==================-->
 
-            <div class="post-featured-image">
-              <a href="<?php the_permalink(); ?>">
-                <?php  the_post_thumbnail('medium'); ?>
-              </a>
-            </div>
-
-            <h3>
-              <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-              </a>
-            </h3>
-
-            <p class="post-info"><?php echo "Published " . get_the_date(); echo " | "; echo "Written by: " . get_the_author(); ?></p>
-            <?php the_excerpt(); ?>
-
-            <a class="btn btn-primary btn-sm" href="<?php the_permalink(); ?>">Read More</a>
-          </div>
-
-
-        <?php } // ends while loop
-      } // ends if statement
-      ?>
-    </div>
-  </div>
+  <section class="row justify-content-center">
+    <?php if(have_posts()){
+      while(have_posts()){
+        the_post(); ?>
+        <div class="grid">
+          <a href="<?php the_permalink(); ?>">
+            <figure class="effect-steve">
+              <?php  the_post_thumbnail('medium'); ?>
+              <figcaption>
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_excerpt(); ?></p>
+              </figcaption>
+            </figure>
+          </a>
+        </div>
+      <?php } // ends while loop
+    } // ends if statement
+    ?>
+    </section>
 
 <?php get_footer(); ?>
