9951a5db21e905ba34794ac0d6654be270eb32d9
Teddi Woodson <teddiwood97@ufl.edu>
2020-05-31 00:30:54 -0400

    Update images

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
     		<div class="card mt-5 mb-5 border-0" style="max-width: 100%;">
     	  	<div class="row no-gutters">
     	    	<div class="col-md-4">
-    	      	<img src="<?php echo get_template_directory_uri(); ?>img/headshot.jpg" class="about card-img" alt="...">
+    	      	<img src="<?php echo get_template_directory_uri(); ?>/img/headshot.jpg" class="about card-img" alt="...">
     	    	</div>
     		    <div class="col-md-8">
     		      <div class="card-body">
@@ -69,7 +69,7 @@
     						  	<div class="progress-bar bg-success-green" role="progressbar" style="width: 88%" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100"></div>
     							</div>
     						</section>
-    						<a href="<?php echo get_template_directory_uri(); ?>img/resume.jpg" target="_blank" type="button" class="btn btn-primary btn-lg">Resume</a>
+    						<a href="<?php echo get_template_directory_uri(); ?>/img/resume.jpg" target="_blank" type="button" class="btn btn-primary btn-lg">Resume</a>
     		      </div>
     		    </div>
     	  	</div>
@@ -79,21 +79,21 @@
     <div class="">
     	<div class="card-deck mt-5">
     		<div class="card">
-    		  <img src="<?php echo get_template_directory_uri(); ?>img/graphic.png" class="card-img-top" alt="...">
+    		  <img src="<?php echo get_template_directory_uri(); ?>/img/graphic.png" class="card-img-top" alt="...">
     		  <div class="card-body">
     		    <h5 class="card-title">Graphic Design</h5>
     		    <p class="card-text">Blurb</p>
     		  </div>
     		</div>
     		<div class="card">
-    		  <img src="<?php echo get_template_directory_uri(); ?>img/web.png" class="card-img-top" alt="...">
+    		  <img src="<?php echo get_template_directory_uri(); ?>/img/web.png" class="card-img-top" alt="...">
     		  <div class="card-body">
     		    <h5 class="card-title">Web Development</h5>
     		    <p class="card-text">Blurb</p>
     		  </div>
     		</div>
     		<div class="card">
-    		<img src="<?php echo get_template_directory_uri(); ?>img/write.png" class="card-img-top" alt="...">
+    		<img src="<?php echo get_template_directory_uri(); ?>/img/write.png" class="card-img-top" alt="...">
     		  <div class="card-body">
     		    <h5 class="card-title">Copy Writing</h5>
     		    <p class="card-text">Blurb.</p>
@@ -102,21 +102,21 @@
     	</div>
     	<div class="card-deck mt-5">
     		<div class="card">
-    		  <img src="<?php echo get_template_directory_uri(); ?>img/brand.png" class="card-img-top" alt="...">
+    		  <img src="<?php echo get_template_directory_uri(); ?>/img/brand.png" class="card-img-top" alt="...">
     		  <div class="card-body">
     		    <h5 class="card-title">Branding</h5>
     		    <p class="card-text">Blurb</p>
     		  </div>
     		</div>
     		<div class="card">
-    		  <img src="<?php echo get_template_directory_uri(); ?>img/project.png" class="card-img-top" alt="...">
+    		  <img src="<?php echo get_template_directory_uri(); ?>/img/project.png" class="card-img-top" alt="...">
     		  <div class="card-body">
     		    <h5 class="card-title">Project Management</h5>
     		    <p class="card-text">Blurb</p>
     		  </div>
     		</div>
     		<div class="card">
-    			<a rel="shrink" class="shrink"><img src="<?php echo get_template_directory_uri(); ?>img/seo.png" class="card-img-top" alt="...">
+    			<a rel="shrink" class="shrink"><img src="<?php echo get_template_directory_uri(); ?>/img/seo.png" class="card-img-top" alt="...">
     			  <div class="card-body">
     			    <h5 class="card-title">SEO</h5>
     			    <p class="card-text">Blurb.</p>
