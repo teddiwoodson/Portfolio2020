e32a97e7ce6c34b3cd66a60b2d38cd8bceeb9b9a
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-03 23:34:03 -0400

    Update jumbotron to original

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
     <!--======================= Header ====================-->
     	<main class="">
-    		<div class="jumbotron jumbotron-fluid">
-    		<div class="container">
-    			<p class="lead"><div class="welcome">
-    				<h1>Hi, Im Teddi.</h1>
-    				<p>Designer. Developer. Creator.</p>
-    				<a href="#" type="button" class="btn btn-primary btn-lg">▼</a>
-    			</div></p>
-    		</div>
-    	</div>
+        <div class="jumbotron jumbotron-fluid">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+</div>
     	</main>
 
     <!--======================= About ====================-->
