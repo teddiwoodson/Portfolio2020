dd95a862efb63c2e999d9a56f4cbaeefce523854
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-03 21:55:51 -0400

    Update navigation

--- header.php
+++ header.php
@@ -23,5 +23,48 @@
 
 <body class="container-fluid">
 <header>
+  <?php if(get_header_image() == '') {?>
+      <h1><a href="<?php echo get_home_url();?>"><?php bloginfo('name');?></a></h1><?php
+    }else {?>
+      <a href="<?php echo get_home_url();?>"><img src="<?php header_image();?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" atl="logo" /></a>
+
+    <?php } ?>
+
+  <!--    Made by Erik Terwan    -->
+<!--   24th of November 2015   -->
+<!--        MIT License        -->
+<nav role='navigation'>
+  <div id="menuToggle">
+    <!--
+    A fake / hidden checkbox is used as click reciever,
+    so you can use the :checked selector on it.
+    -->
+    <input type="checkbox" />
+
+    <!--
+    Some spans to act as a hamburger.
+
+    They are acting like a real hamburger,
+    not that McDonalds stuff.
+    -->
+    <span></span>
+    <span></span>
+    <span></span>
+
+    <!--
+    Too bad the menu has to be inside of the button
+    but hey, it's pure CSS magic.
+    -->
+    <div id="menu">
+      <?php
+        if(has_nav_menu('top-menu')) {
+          wp_nav_menu(array('theme_location' => 'top-menu', 'container_class' => 'top-menu-class'));
+        } else{
+          echo "Please select a top menu through the dashboard";
+        }
+      ?>
+    </div>
+  </div>
+</nav>
 
 </header>
