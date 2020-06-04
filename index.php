6f0a1ac30ec4b6be62729a9e97174a29ba07da87
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-03 23:23:02 -0400

    Update hover over styles

--- index.php
+++ index.php
@@ -101,20 +101,25 @@
 
 
     <!-- ======================= Blog Posts ==================-->
-<section class="row justify-content-center">
-  <?php if(have_posts()){ while(have_posts()){the_post(); ?>
-  <div class="grid">
-    <figure class="effect-steve">
-      <?php  the_post_thumbnail('medium'); ?>
-      <figcaption>
-        <a href="<?php the_permalink(); ?>">
-      </figcaption>
-    </figure>
-  </div>
-      <?php } // ends while loop
-    } // ends if statement
-    ?>
-</section>
+    <section class="row justify-content-center">
+      <?php if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+          <div class="grid">
+            <a href="<?php the_permalink(); ?>">
+              <figure class="effect-ming">
+                <?php  the_post_thumbnail('medium'); ?>
+                <figcaption>
+                  <h2><?php the_title(); ?></h2>
+                  <p><?php the_excerpt(); ?></p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        <?php } // ends while loop
+      } // ends if statement
+      ?>
+      </section>
 
     <!--======================= Resume ====================-->
 
