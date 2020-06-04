75de5f60a16aadc9267ce57fa5ad50019e28d33b
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-04 19:24:00 -0400

    Update arrows sizing

--- index.php
+++ index.php
@@ -121,7 +121,7 @@
         </div>
       </div>
     </div>
-    <div class="arrows col-12">
+    <div class="arrows col-3">
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
