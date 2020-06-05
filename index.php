cb40279b8966ef323981a2f641e0f957e8278793
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 12:40:59 -0400

    Remove parallax

--- index.php
+++ index.php
@@ -94,14 +94,12 @@
   </div>
 </div>
 
-<div class="bg">
   <div class="jumbotron2 jumbotron-fluid d-flex justify-content-center align-items-center">
     <div class="container">
       <h2 class="display-4">Fluid jumbotron</h2>
       <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
   </div>
-</div>
 
 
 <!-- ======================= Skills ==================-->
