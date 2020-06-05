415effae6bd5c9005ca03e0a984e89852bb87e7b
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 16:09:12 -0400

    Remove first jumbo2

--- index.php
+++ index.php
@@ -109,13 +109,6 @@
   </div>
 </section>
 
-  <div class="jumbotron2 jumbotron-fluid d-flex justify-content-center align-items-center">
-    <div class="container">
-      <h2 class="display-4">Fluid jumbotron</h2>
-      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-    </div>
-  </div>
-
 
 <!-- ======================= Skills ==================-->
 <div class="container">
