c772c67b491fa486a23ac7b12b6100a0b778c472
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-04 20:00:29 -0400

    attempt horizontal scrolling

--- index.php
+++ index.php
@@ -136,7 +136,40 @@
   </div>
 
 </section>
-
+<div class="scrolling-wrapper">
+  <div class="card">
+    <div class="card-body"  >
+      <h3 class="card-title">Card title</h3>
+      <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+    <div class="card">
+      <div class="card-body"  >
+        <h3 class="card-title">Card title</h3>
+        <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+      <div class="card">
+        <div class="card-body"  >
+          <h3 class="card-title">Card title</h3>
+          <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+        <div class="card">
+          <div class="card-body"  >
+            <h3 class="card-title">Card title</h3>
+            <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+</div>
     <!--======================= Services ====================
     <div class="">
     	<div class="card-deck mt-5">
