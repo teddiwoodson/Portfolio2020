1247b282bc1f129701a0608c04c4187a0283a1a4
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 00:55:27 -0400

    Update jumbotron fonts

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 
     <div class="jumbotron2 jumbotron-fluid d-flex justify-content-center align-items-center">
       <div class="container">
-        <h1 class="display-4">Fluid jumbotron</h1>
+        <h2 class="display-4">Fluid jumbotron</h2>
         <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
       </div>
     </div>
@@ -78,7 +78,7 @@
 
 <div class="jumbotron2 jumbotron-fluid d-flex justify-content-center align-items-center">
   <div class="container">
-    <h1 class="display-4">Fluid jumbotron</h1>
+    <h2 class="display-4">Fluid jumbotron</h2>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
   </div>
 </div>
