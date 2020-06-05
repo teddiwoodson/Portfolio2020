6cdc9db2e3507543a6601e3270cc922e9c39fe67
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-04 20:06:34 -0400

    Attempt horizontal scroll again

--- index.php
+++ index.php
@@ -145,30 +145,43 @@
       <a href="#" class="card-link">Card link</a>
       <a href="#" class="card-link">Another link</a>
     </div>
-    <div class="card">
-      <div class="card-body"  >
-        <h3 class="card-title">Card title</h3>
-        <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-      <div class="card">
-        <div class="card-body"  >
-          <h3 class="card-title">Card title</h3>
-          <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-        <div class="card">
-          <div class="card-body"  >
-            <h3 class="card-title">Card title</h3>
-            <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-          </div>
+  </div>
+  <div class="card">
+    <div class="card-body"  >
+      <h3 class="card-title">Card title</h3>
+      <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body"  >
+      <h3 class="card-title">Card title</h3>
+      <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body"  >
+      <h3 class="card-title">Card title</h3>
+      <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body"  >
+      <h3 class="card-title">Card title</h3>
+      <h6 class="card-subtitle mb-2 text-muted">Goodbye</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
 </div>
     <!--======================= Services ====================
     <div class="">
