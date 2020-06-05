4e31ac9ad11b326886e0b90594d7f63071f1f604
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 14:12:55 -0400

    Update nav

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 <body class="container-fluid">
 <header>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg col-12">
   <a class="" href="#"><h2>Teddi Woodson</h2></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
