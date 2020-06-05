d3924539583cb0dd27ed9e3fa025155bc9be5ca4
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 02:50:46 -0400

    Update divider

--- index.php
+++ index.php
@@ -36,11 +36,19 @@
     </div>
 
 <!-- ======================= Horizontal Card ==================-->
-    <div class="card mb-3">
-  <div class="row no-gutters">
+<div class="card mb-5 mt-5">
+  <div class="row">
     <div class="col-md-6">
-      <div class="card-body">
-        <div class="accordion" id="accordionExample">
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+    </div>
+    <div class="col-md-6">
+      <div class="accordion" id="accordionExample">
   <div class="card">
     <div class="card-header" id="headingOne">
       <h2 class="mb-0">
@@ -85,10 +93,7 @@
     </div>
   </div>
 </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/background3.jpg" class="about card-img" alt="...">
+
     </div>
   </div>
 </div>
