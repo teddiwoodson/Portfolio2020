05413754856e9d155e27fdea7bbfcaee1e1967fa
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 22:29:30 -0400

    Update jumbotron2

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
       </div>
     </div>
     <div class="divider-img col-md-5">
-      <div class="jumbotron jumbotron-fluid">
+      <div class="jumbotron2 jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Fluid jumbotron</h1>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
