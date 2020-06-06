20d012880d7ab16cd06a5cb8f2c5674967f9b4c4
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-05 20:22:42 -0400

    Update jumbotron

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
     	<main class="">
         <div class="jumbotron jumbotron-fluid d-flex justify-content-center align-items-center">
           <div class="jumbo-content">
-          <h1>Hello, world!</h1>
-          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <h1>TEDDI WOODSON</h1>
+          <p class="lead">Designer. Developer. Creator.</p>
           <hr class="my-4">
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
           <p class="lead">
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
           </p>
