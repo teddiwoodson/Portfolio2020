46b5ca3ba1e2eca45ea155e466c8051192485de5
Teddi Woodson <teddiwood97@ufl.edu>
2020-06-07 19:31:57 -0400

    Add favicon

--- header.php
+++ header.php
@@ -18,6 +18,8 @@
 
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
+  <link rel="shortcut icon"  href="<?php echo get_template_directory_uri(); ?>/img/favicon.png" type="image/png" />
+
   <?php wp_head(); ?>
 </head>
 
